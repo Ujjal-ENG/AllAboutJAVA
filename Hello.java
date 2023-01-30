fcc7fc846044d0f998f2b8014af59d355d3c3268
user <ujjalroy7862@gmail.com>
2023-01-30 15:39:05 +0600

    condition checking

--- Hello.java
+++ Hello.java
@@ -1,9 +1,15 @@
 class Hello {
   public static void main(String[] args) {
-    
+
     int num1 = 3;
     int num2 = 4;
-    System.out.println(num1 + num2);
+    
+    
+    if (num1 == num2) {
+      System.out.println("number is equal");
+    } else {
+      System.out.println("Number is not equal");
+    }
   }
 
 }
