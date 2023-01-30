d72d4014a7eba44282b9d470b5cd1af5c1f476ae
user <ujjalroy7862@gmail.com>
2023-01-30 17:57:58 +0600

    this the nested for loop

--- Hello.java
+++ Hello.java
@@ -1,20 +1,19 @@
 class Hello {
   public static void main(String[] args) {
 
-    int n = 2;
-
-    switch (n) {
-      case 1:
-        System.out.println("Number is one");
-        break;
-      
-      case 2:
-        System.out.println("Number is two");
-        break;
-      default:
-      System.out.println("Kono kisu dei nai");
-    }
+    int i = 1;
     
-  }
+    while (i <= 4) {
+      System.out.println("Hi = " + i);
+      
+      int j = 1;
 
+      while (j <= 3) {
+        System.out.println("Inner Loop Hi = " + j);
+        j++;
+      }
+
+      i++;
+    }
+  }
 }
