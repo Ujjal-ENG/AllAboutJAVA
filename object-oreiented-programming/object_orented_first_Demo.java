9a34b7db7c1a1580abd2064b6f1288dc144ce438
user <ujjalroy7862@gmail.com>
2023-01-30 19:18:42 +0600

    done the 1st part of the object oreiented programming

---
+++ object-oreiented-programming/object_orented_first_Demo.java
@@ -0,0 +1,21 @@
+
+class Calculator {
+  public int add(int n1,int n2) {
+   
+    int r = n1 + n2;
+    return r;
+  }
+}
+
+public class object_orented_first_Demo {
+  public static void main(String[] args) {
+    int num1 = 4;
+    int num2 = 5;
+
+    Calculator calc = new Calculator();
+
+    int res = calc.add(num1, num2);
+    
+    System.out.println(res);
+  }
+}
