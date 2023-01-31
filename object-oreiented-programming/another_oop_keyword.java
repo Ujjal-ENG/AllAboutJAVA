a8f9ad15a0e9008b6529c6a1febf5f56e7bd749e
user <ujjalroy7862@gmail.com>
2023-02-01 01:46:13 +0600

    now lerant the super keyword use cases

--- object-oreiented-programming/another_oop_keyword.java
+++ object-oreiented-programming/another_oop_keyword.java
@@ -3,17 +3,27 @@ class A {
   public A() {
     System.out.println("In A");
   }
+
+  public A(int a) {
+    System.out.println("In a paramenter");
+  }
 }
 
 class B extends A {
   public B() {
     System.out.println("In B");
   }
+
+  public B(int b) {
+    super(b);
+    System.out.println("In b parameter");
+  }
+  
 }
 
 
 public class another_oop_keyword {
   public static void main(String[] args) {
-    B obj = new B();
+    B obj = new B(4);
   }
 }
