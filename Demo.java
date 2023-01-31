119ebf6472c16d1d32ba50a48f366fd33079b013
user <ujjalroy7862@gmail.com>
2023-02-01 01:11:31 +0600

    learn new static method use cases

--- Demo.java
+++ Demo.java
@@ -15,6 +15,10 @@ class Mobile {
   public void show() {
     System.out.println("Brand Name: " + brand + "\nPrice: " + price + "\nName: " + name);
   }
+
+  public static void show1(Mobile ob) {
+    System.out.println("Brand Name: " + ob.brand + "\nPrice: " + ob.price + "\nName: " + name);
+  }
   
 }
 
@@ -22,5 +26,6 @@ public class Demo {
   public static void main(String[] args) {
     Mobile mb1 = new Mobile("Realme", 26500);
     mb1.show();
+    mb1.show1();
   }
 }
