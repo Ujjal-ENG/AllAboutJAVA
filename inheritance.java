0f1c8e7f5d94bef933ece9024adf481702e62167
user <ujjalroy7862@gmail.com>
2023-02-01 02:17:45 +0600

    learn the new things in java

---
+++ inheritance.java
@@ -0,0 +1,20 @@
+class Calc {
+  public int add(int n1, int n2) {
+    return n1 + n2;
+  }
+
+  public int sub(int n1, int n2) {
+    return (n1 - n2) < 0 ? 0 : n1 - n2;
+  }
+}
+
+public class inheritance {
+  public static void main(String[] args) {
+    Calc c1 = new Calc();
+
+    int r1 = c1.add(8, 7);
+    int r2 = c1.sub(7, 3);
+
+    System.out.println(r1+"and"+r2);
+  }
+}
