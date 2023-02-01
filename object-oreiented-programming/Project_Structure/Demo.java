6f199fac657097be54043905978c083c4e1bec58
user <ujjalroy7862@gmail.com>
2023-02-01 18:14:06 +0600

    not working yet in package seciton

--- object-oreiented-programming/Project_Structure/Demo.java
+++ object-oreiented-programming/Project_Structure/Demo.java
@@ -1,5 +1,15 @@
 package Project_Structure;
 
+import Project_Structure.Tools_Package.Calc;
+
 public class Demo {
-  
+  public static void main(String[] args) {
+
+    Calc c1 = new Calc();
+    
+    int r1 = c1.add(8, 7);
+    int r2 = c1.sub(3, 7);
+
+    System.out.println(r1+" "+r2);
+  }
 }
